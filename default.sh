0f96f076b60397e55771e9030e5918709146db0b
PEPEPGADEV <121109042+PEPEPGADEV@users.noreply.github.com>
2026-03-15 23:27:50 +0300

    Update default.sh

--- default.sh
+++ default.sh
@@ -35,7 +35,7 @@ CHECKPOINT_MODELS=(
 )
 
 UNET_MODELS=(
-    "https://civitai.com/api/download/models/2753918?token=098560db733d2419cd61b2347adf7f7a"
+    "https://huggingface.co/circlestone-labs/Anima/resolve/main/split_files/diffusion_models/anima-preview2.safetensors"
 )
 
 LORA_MODELS=(
