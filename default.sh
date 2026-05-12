389f7220a0f3bded835f0bf449753071a177f1be
PEPEPGADEV <121109042+PEPEPGADEV@users.noreply.github.com>
2026-05-12 12:12:49 +0300

    Update default.sh

--- default.sh
+++ default.sh
@@ -36,11 +36,10 @@ CHECKPOINT_MODELS=(
 )
 
 UNET_MODELS=(
-    "https://civitai.red/api/download/models/2932616?token=098560db733d2419cd61b2347adf7f7a"
+    "https://civitai.red/api/download/models/2851312?token=098560db733d2419cd61b2347adf7f7a"
 )
 
 LORA_MODELS=(
-    "https://civitai.red/api/download/models/2915112?token=098560db733d2419cd61b2347adf7f7a"
 )
 
 CLIP_MODELS=(
